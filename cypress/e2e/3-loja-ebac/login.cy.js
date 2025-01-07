74814f3efc143589e87f4b4c1b1a4c2516f5cc4d
Kelly cavalcante <kellycristinasouto@gmail.com>
2025-01-07 16:55:48 -0300

    Ocultado dados  de login

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,10 @@
 /// <references types ="cypress"/>
+const perfil= require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () =>{
     beforeEach(() => {
         
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     afterEach(() => {
@@ -34,4 +35,21 @@ describe('Funcionalidade: Login', () =>{
         cy.get('.woocommerce-error').should('exist')
 
     });
+    it('Deve fazer login com sucesso usando massa de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, kelly.teste1 (não é kelly.teste1? Sair)')
+    });
+
+it.only('Deve fazer login com sucesso - usando fixture', () => {
+    cy.fixture('perfil').then(dados =>{
+        cy.get('#username').type(dados.usuario , {log: false})
+        cy.get('#password').type(dados.senha , {log: false})
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' ,'Olá, kelly.teste1 (não é kelly.teste1? Sair)')
+    })
+
+});
+  
 })
